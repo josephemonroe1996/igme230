c236d969384daa64beacb9f1e617be367e954c27
Joe Monroe <josephemonroe1996@gmail.com>
2018-12-06 12:44:14 -0500

    finished practical

--- samplePractical/fall2018sample.js
+++ samplePractical/fall2018sample.js
@@ -1,1 +1,10 @@
 /* Don't use <script> tags in a linked js file! */
+
+$(".submenu").hide();
+$(".menuitem").click(function(){
+    $(this).next().slideToggle();
+});
+$("#content").load("content1.txt");
+$("#choose-content").change(function(){
+    $("#content").load($(this).val());
+});
