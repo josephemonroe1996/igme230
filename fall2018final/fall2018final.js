01fa5c943f7b8ec51e80de9a7707a20d8d5a0a8e
Joseph Monroe (RIT Student) <jem1229@ad.rit.edu>
2018-12-06 15:12:14 -0500

    added changes to final

--- fall2018final/fall2018final.js
+++ fall2018final/fall2018final.js
@@ -1,1 +1,9 @@
 $(".list").hide(); /*hides submenu items on page load */
+$(".top").click(function(){ /* slide menu on click */
+    $(this).next().slideToggle();
+});
+
+$("article").load("article0.txt"); /*loads content on page start */
+$("radio").change(function(){ /*loads content on change */
+    $("article").load($(this).val());
+});
