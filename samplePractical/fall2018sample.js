d9d58368a6dd20e71b689106c6e27bc28d8f9894
Joseph Monroe (RIT Student) <jem1229@ad.rit.edu>
2018-12-06 13:54:15 -0500

    added comments

--- samplePractical/fall2018sample.js
+++ samplePractical/fall2018sample.js
@@ -1,10 +1,10 @@
 /* Don't use <script> tags in a linked js file! */
 
-$(".submenu").hide();
-$(".menuitem").click(function(){
+$(".submenu").hide(); /*hides submenu items on page load */
+$(".menuitem").click(function(){ /* slide menu on click */
     $(this).next().slideToggle();
 });
-$("#content").load("content1.txt");
-$("#choose-content").change(function(){
+$("#content").load("content1.txt"); /*loads content on page start */
+$("#choose-content").change(function(){ /*loads content on change */
     $("#content").load($(this).val());
 });
