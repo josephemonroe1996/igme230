27a47ee69392b0ba6d758a1de59c3a02de25e44b
josephemonroe1996 <42783664+josephemonroe1996@users.noreply.github.com>
2018-12-10 07:42:26 -0500

    Delete interactive.js

--- portfolio/interactive.js
+++
@@ -1,5 +0,0 @@
-//fades in page on start
-$(document).ready(function(){$("header").hide().fadeIn(1000);});
-$(document).ready(function(){$("article").hide().fadeIn(3000);});
-$(document).ready(function(){$("nav").hide().fadeIn(3500);});
-$(document).ready(function(){$("aside").hide().fadeIn(4000);});
